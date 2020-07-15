c73cfb222cf726f951283a45fc28a4805474d585
Victor Ivanov <3826872+vmivanov@users.noreply.github.com>
2020-07-15 11:40:27 +0100

    zsh: Add dotfile for FontAwesome 5.x icons in Powerline10k

---
+++ zsh/.p10k_fa5.zsh
@@ -0,0 +1,16 @@
+#
+# FontAwesome 5.x icons
+# 
+typeset -g POWERLEVEL9K_HOME_ICON='\uF015 '$s                  # 
+typeset -g POWERLEVEL9K_FOLDER_ICON='\uF07B'$s                 # 
+typeset -g POWERLEVEL9K_SWAP_ICON='\uF3FD'$s                   # 
+typeset -g POWERLEVEL9K_RAM_ICON='\uF3FD'$s                    # 
+typeset -g POWERLEVEL9K_VCS_INCOMING_CHANGES_ICON='\uF0AB '    # 
+typeset -g POWERLEVEL9K_VCS_OUTGOING_CHANGES_ICON='\uF0AA '    # 
+typeset -g POWERLEVEL9K_VCS_BOOKMARK_ICON='\uF4AD '            # 
+typeset -g POWERLEVEL9K_VCS_GIT_BITBUCKET_ICON='\uF171 '       # 
+typeset -g POWERLEVEL9K_VCS_GIT_GITHUB_ICON='\uF09B '          # 
+typeset -g POWERLEVEL9K_VCS_GIT_GITLAB_ICON='\uF296 '          # 
+typeset -g POWERLEVEL9K_PYTHON_ICON='\uF3E2'$s                 # 
+
+# vim: ft=zsh ts=4 sts=4 sw=4 tw=100 et
